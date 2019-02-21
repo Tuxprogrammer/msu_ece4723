631302153d063a6e284845f4583be5a950a41730
Will Carroll <woc17@msstate.edu>
2019-02-20 18:58:50 -0600

    added esos_sensor.c to the shared lib

--- SCons_esos.py
+++ SCons_esos.py
@@ -37,7 +37,8 @@ SHARED_LIB_COPY = [
     ("../../lab_3/src/esos_f14ui.c", "../../lib/src/esos_f14ui.c"),
     ("../../lab_4/include/revF14.h", "../../lib/include/revF14.h"),
     ("../../lab_4/include/esos_pic24_sensor.h", "../../lib/include/esos_pic24_sensor.h"),
-    ("../../lab_4/src/esos_pic24_sensor.c", "../../lib/src/esos_pic24_sensor.c")
+    ("../../lab_4/src/esos_pic24_sensor.c", "../../lib/src/esos_pic24_sensor.c"),
+    ("../../lab_4/src/esos_sensor.c", "../../lib/src/esos_sensor.c")
 ]
 
 SHARED_LIB_NOTE = \
