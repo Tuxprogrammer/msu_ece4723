db4d9804c4c83b3bb6d2dfef3d98ad0af5fa8964
Spencer Callicott <sc2257@msstate.edu>
2019-02-27 19:28:19 -0600

    Fix the filenaming and some typos, as well as scons_esos.py

--- SCons_esos.py
+++ SCons_esos.py
@@ -38,7 +38,9 @@ SHARED_LIB_COPY = [
     ("../../lab_4/include/revF14.h", "../../lib/include/revF14.h"),
     ("../../lab_4/include/esos_pic24_sensor.h", "../../lib/include/esos_pic24_sensor.h"),
     ("../../lab_4/src/esos_pic24_sensor.c", "../../lib/src/esos_pic24_sensor.c"),
-    ("../../lab_4/src/esos_sensor.c", "../../lib/src/esos_sensor.c")
+    ("../../lab_4/src/esos_sensor.c", "../../lib/src/esos_sensor.c"),
+    ("../../lab_5/include/esos_pic24_lcd44780.h", "../../lib/include/esos_pic24_lcd44780.h"),
+    ("../../lab_5/src/esos_pic24_lcd44780.c", "../../lib/src/esos_pic24_lcd44780.c")
 ]
 
 SHARED_LIB_NOTE = \
@@ -78,6 +80,7 @@ ESOS_LIB_FILES = ["../../../pic24lib_all/esos/src/esos.c",
                   "../../../pic24lib_all/esos/src/esos_cb.c",
                   "../../../pic24lib_all/esos/src/esos_mail.c",
                   # "../../../pic24lib_all/esos/src/esos_sensor.c",
+                  "../../../pic24lib_all/esos/src/esos_lcd44780.c",
                   "../../../pic24lib_all/esos/src/pic24/esos_pic24_i2c.c",
                   "../../../pic24lib_all/esos/src/pic24/esos_pic24_irq.c",
                   "../../../pic24lib_all/esos/src/pic24/esos_pic24_rs232.c",
