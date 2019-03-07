ebd0fb389c3b0fbad64954a2ee2b0a2b5e41cd4e
Will Carroll <woc17@msstate.edu>
2019-03-07 11:26:28 -0600

    autoformatted all files

--- lab_6/include/esos_menu.h
+++ lab_6/include/esos_menu.h
@@ -6,55 +6,55 @@
 // A single item for a longmenu, see below.
 // Contains lines 1 and 2 of text.
 typedef struct {
-	char ac_line1[8];
-	char ac_line2[8];
+    char ac_line1[8];
+    char ac_line2[8];
 } esos_menu_longmenu_item_t;
 
-/* 
+/*
 esos_menu_longmenu_t Usage:
-	u8_numitems: The number of items contained in the array ast_items[]
-	u8_choice:	 Our currently selected item (index) contained in ast_items[]
-	ast_items[]: esos_menu_longmenu_item_t structure containing our 2 lines
-				 to display for each menu item in ast_items[]
+    u8_numitems: The number of items contained in the array ast_items[]
+    u8_choice:	 Our currently selected item (index) contained in ast_items[]
+    ast_items[]: esos_menu_longmenu_item_t structure containing our 2 lines
+                 to display for each menu item in ast_items[]
 */
 typedef struct {
-	uint8_t u8_numitems;
-	uint8_t u8_choice;
-	esos_menu_longmenu_item_t ast_items[];
+    uint8_t u8_numitems;
+    uint8_t u8_choice;
+    esos_menu_longmenu_item_t ast_items[];
 } esos_menu_longmenu_t;
 
 /*
 esos_menu_staticmenu_t Usage:
-	u8_numlines: 	The number of lines contained in 2D array lines[][8]
-	u8_currentline: The currently selected line in 2D array lines[][8]
-	lines[][8]:		Contains an array of lines to be displayed to the LCD
+    u8_numlines: 	The number of lines contained in 2D array lines[][8]
+    u8_currentline: The currently selected line in 2D array lines[][8]
+    lines[][8]:		Contains an array of lines to be displayed to the LCD
 */
 typedef struct {
-	uint8_t u8_numlines;
-	uint8_t u8_currentline;
-	char lines[][8];
+    uint8_t u8_numlines;
+    uint8_t u8_currentline;
+    char lines[][8];
 } esos_menu_staticmenu_t;
 /*
 esos_menu_entry_item_t Usage:
-	u8_value: 		The actual value being edited by the user.
-	u8_min: 		The minimum value the user is allowed to enter.
-	u8_max:			The maximum value the user is allowed to enter.
+    u8_value: 		The actual value being edited by the user.
+    u8_min: 		The minimum value the user is allowed to enter.
+    u8_max:			The maximum value the user is allowed to enter.
 */
 typedef struct {
-	char label[8];
-	int16_t value;
-	int16_t min;
-	int16_t max;
+    char label[8];
+    int16_t value;
+    int16_t min;
+    int16_t max;
 } esos_menu_entry_item_t;
 
 /*
 esos_menu_entry_t Usage:
-	entries[]: Contains the two visible entries on your screen.
-			   ex: x = 147
-				   y = 256
+    entries[]: Contains the two visible entries on your screen.
+               ex: x = 147
+                   y = 256
 */
 typedef struct {
-	esos_menu_entry_item_t entries[2];
+    esos_menu_entry_item_t entries[2];
 } esos_menu_entry_t;
 
 // Call this to initialize the cherrymenu subsystem.
@@ -62,42 +62,44 @@ void esos_menu_init(void);
 
 // Call these to run and wait for a menu of your creation, passing the
 // appropriate structure in.
-#define ESOS_TASK_WAIT_ESOS_MENU_LONGMENU(structure) do { \
-	esos_menu_longmenu_t *ps_menu = &structure; \
-	__esos_menu_conf.e_menutype = LONGMENU; \
-	__esos_menu_conf.pv_data = ps_menu; \
-	ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE); \
-} while(0)
+#define ESOS_TASK_WAIT_ESOS_MENU_LONGMENU(structure)                                                                   \
+    do {                                                                                                               \
+        esos_menu_longmenu_t *ps_menu = &structure;                                                                    \
+        __esos_menu_conf.e_menutype = LONGMENU;                                                                        \
+        __esos_menu_conf.pv_data = ps_menu;                                                                            \
+        ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE);                                                     \
+    } while (0)
 
-#define ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(structure) do { \
-	esos_menu_staticmenu_t *ps_menu = &structure; \
-	__esos_menu_conf.e_menutype = STATICMENU; \
-	__esos_menu_conf.pv_data = ps_menu; \
-	ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE); \
-} while(0)
+#define ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(structure)                                                                 \
+    do {                                                                                                               \
+        esos_menu_staticmenu_t *ps_menu = &structure;                                                                  \
+        __esos_menu_conf.e_menutype = STATICMENU;                                                                      \
+        __esos_menu_conf.pv_data = ps_menu;                                                                            \
+        ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE);                                                     \
+    } while (0)
 
-#define ESOS_TASK_WAIT_ESOS_MENU_ENTRY(structure) do { \
-	esos_menu_entry_t *ps_menu = &structure; \
-	__esos_menu_conf.e_menutype = ENTRY; \
-	__esos_menu_conf.pv_data = ps_menu; \
-	ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE); \
-} while(0)
+#define ESOS_TASK_WAIT_ESOS_MENU_ENTRY(structure)                                                                      \
+    do {                                                                                                               \
+        esos_menu_entry_t *ps_menu = &structure;                                                                       \
+        __esos_menu_conf.e_menutype = ENTRY;                                                                           \
+        __esos_menu_conf.pv_data = ps_menu;                                                                            \
+        ESOS_TASK_WAIT_UNTIL(__esos_menu_conf.e_menutype == NONE);                                                     \
+    } while (0)
 
 // Prototypes and definitions below this line are not part of the stable
 // API and should not be used by clients.  Here be dragons.
-
 
 /*Notes: Since *pv_data is void, the value pv_data can point to any
   of our menu types.
 */
 typedef struct {
-	enum {
-		NONE,
-		LONGMENU,
-		STATICMENU,
-		ENTRY,
-	} e_menutype;
-	void *pv_data;
+    enum {
+        NONE,
+        LONGMENU,
+        STATICMENU,
+        ENTRY,
+    } e_menutype;
+    void *pv_data;
 } __esos_menu_conf_t;
 
 extern __esos_menu_conf_t __esos_menu_conf;
--- lab_6/src/fall17lib.c
+++ lab_6/src/fall17lib.c
@@ -12,110 +12,110 @@ static const char *conversionchars = "zyxwvutsrqponmlkjihgfedcba9876543210123456
  * Released under GPLv3.
  * Modified from http://www.strudel.org.uk/itoa
  */
-char* itoa(int value, char* result, int base) {
-	char* ptr = result, *ptr1 = result, tmp_char;
-	int tmp_value;
-
-	// check that the base if valid
-	if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-	do {
-		tmp_value = value;
-		value /= base;
-		*ptr++ = conversionchars[35 + (tmp_value - value * base)];
-	} while ( value );
-
-	// Apply negative sign
-	if (tmp_value < 0) *ptr++ = '-';
-	*ptr-- = '\0';
-	while(ptr1 < ptr) {
-		tmp_char = *ptr;
-		*ptr--= *ptr1;
-		*ptr1++ = tmp_char;
-	}
-	return result;
-}
-
-char* utoa(unsigned int value, char* result, int base) {
-	char* ptr = result, *ptr1 = result, tmp_char;
-	unsigned int tmp_value;
-
-	// check that the base is valid
-	if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-	do {
-		tmp_value = value;
-		value /= base;
-		*ptr++ = conversionchars[35 + (tmp_value - value * base)];
-	} while ( value );
-
-	*ptr-- = '\0';
-	while(ptr1 < ptr) {
-		tmp_char = *ptr;
-		*ptr--= *ptr1;
-		*ptr1++ = tmp_char;
-	}
-	return result;
-}
-
-char* utoar(unsigned int value, char* result, int base, unsigned int width)
+char *itoa(int value, char *result, int base)
 {
-	unsigned int ui_len;
-	utoa(value, result, base);
+    char *ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
 
-	ui_len = strlen(result);
+    // check that the base if valid
+    if (base < 2 || base > 36) {
+        *result = '\0';
+        return result;
+    }
 
-	// Right-align by shifting (including the null terminator!) and filling with '0's.
-	memmove(result + width - ui_len, result, ui_len + 1);
-	memset(result, '0', width - ui_len);
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = conversionchars[35 + (tmp_value - value * base)];
+    } while (value);
 
-	return result;
+    // Apply negative sign
+    if (tmp_value < 0)
+        *ptr++ = '-';
+    *ptr-- = '\0';
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr-- = *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
 }
 
-char* upperstring(char* psz_str)
+char *utoa(unsigned int value, char *result, int base)
 {
-	while(*psz_str) {
-		*psz_str = toupper(*psz_str);
-		++psz_str;
-	}
+    char *ptr = result, *ptr1 = result, tmp_char;
+    unsigned int tmp_value;
+
+    // check that the base is valid
+    if (base < 2 || base > 36) {
+        *result = '\0';
+        return result;
+    }
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = conversionchars[35 + (tmp_value - value * base)];
+    } while (value);
+
+    *ptr-- = '\0';
+    while (ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr-- = *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
+
+char *utoar(unsigned int value, char *result, int base, unsigned int width)
+{
+    unsigned int ui_len;
+    utoa(value, result, base);
+
+    ui_len = strlen(result);
+
+    // Right-align by shifting (including the null terminator!) and filling with '0's.
+    memmove(result + width - ui_len, result, ui_len + 1);
+    memset(result, '0', width - ui_len);
+
+    return result;
+}
+
+char *upperstring(char *psz_str)
+{
+    while (*psz_str) {
+        *psz_str = toupper(*psz_str);
+        ++psz_str;
+    }
 }
 
 uint8_t lut3pt[] = {
-	0b00000,
-	0b00011,
-	0b00110,
-	0b01001,
-	0b01100,
-	0b10000,
-	0b10011,
-	0b10110,
-	0b11001,
-	0b11100,
+    0b00000, 0b00011, 0b00110, 0b01001, 0b01100, 0b10000, 0b10011, 0b10110, 0b11001, 0b11100,
 };
 
 uint8_t dtou3p5(uint8_t ipart, uint8_t fpart)
 {
-	uint8_t fpart3p5;
+    uint8_t fpart3p5;
 
-	fpart = MIN(fpart, 9);
+    fpart = MIN(fpart, 9);
 
-	return ipart << 5 | lut3pt[fpart];
+    return ipart << 5 | lut3pt[fpart];
 }
 
 uint8_t u3p5toipart(uint8_t fixedpt)
 {
-	return fixedpt >> 5;
+    return fixedpt >> 5;
 }
 
 uint8_t u3p5tofpart(uint8_t fixedpt)
 {
-	uint8_t fpart = (fixedpt & 0b11111);
-	uint8_t i;
+    uint8_t fpart = (fixedpt & 0b11111);
+    uint8_t i;
 
-	for(i = 0; i < NELEM(lut3pt); ++i) {
-		if(lut3pt[i] >= fpart)
-			return i;
-	}
+    for (i = 0; i < NELEM(lut3pt); ++i) {
+        if (lut3pt[i] >= fpart)
+            return i;
+    }
 
-	return 9;
+    return 9;
 }
