d95b8ae223a2ae40422d7bb1c6b22e7d2dca8e3e
Jack Fletcher <jdf469@msstate.edu>
2019-03-10 03:13:17 -0500

    Outline the menu and implement Set LEDs function

--- lab_6/t6_ex.c
+++ lab_6/t6_ex.c
@@ -6,20 +6,18 @@
         by the coding standards.
 */
 static esos_menu_longmenu_t mm = {
-    .u8_numitems = 10,
+    .u8_numitems = 8,
     .u8_choice = 0, // Default
     .ast_items =
         {
-            { "Set", "User" },
-            { "Set", "Port" },
-            { "Set", "Dest" },
-            { "Set", "Temp" },
-            { "Set", "T-stat" },
-            { "Self", "Test" },
-            { "Set", "LEDFP" },
-            { "Static", "Greet" },
-            { "Greet", "UART" },
-            { "", "Back..." },
+            { "Set", "wvform" },
+            { "Set", "freq" },
+            { "Set", "ampltd" },
+            { "Set", "duty" },
+            { "Read", "LM60" },
+            { "Read", "1631" },
+            { "Set", "LEDs" },
+            { "", "About..." },
         },
 };
 
@@ -29,17 +27,59 @@ static esos_menu_staticmenu_t err = {
     .lines = { { "404" }, { "NotFnd" } },
 };
 
-static esos_menu_entry_t ledfp = {
-    .entries[0].label = "Per = ",
-    .entries[0].value = 1000, // Default, per = 1s
-    .entries[0].min = 250,
-    .entries[0].max = 2000,
+// TODO: determine what type of entry the wvform ought to be
+static esos_menu_staticmenu_t wvform = {
+    .u8_numlines = 4,
+    .u8_currentline = 0,
+    .lines = { { "idk" }, { "what" }, { "this" }, { "is" } },
 };
 
-static esos_menu_staticmenu_t greet = {
+static esos_menu_entry_t freq = {
+    .entries[0].label = "Freq = ",
+    .entries[0].value = 1000, // Default, freq = 1s
+    .entries[0].min = 64,
+    .entries[0].max = 2047,
+};
+
+// note ampl is 10 times the value to avoid floats
+static esos_menu_entry_t ampl = {
+    .entries[0].label = "Ampl = ",
+    .entries[0].value = 10, // Default, ampl = 1.0V
+    .entries[0].min = 0,
+    .entries[0].max = 33,
+};
+
+static esos_menu_entry_t duty = {
+    .entries[0].label = "Duty = ",
+    .entries[0].value = 50, // Default, duty cycle = 50%
+    .entries[0].min = 0,
+    .entries[0].max = 100,
+};
+
+static esos_menu_entry_t leds = {
+    .entries[0].label = "LEDs = ",
+    .entries[0].value = 3,
+    .entries[0].min = 0,
+    .entries[0].max = 7,
+};
+
+static esos_menu_staticmenu_t lm60 = {
     .u8_numlines = 2,
     .u8_currentline = 0,
-    .lines = { { "Hello" }, { "friend" } },
+    .lines = { { "Hello" }, { "LM60" } },
+};
+
+static esos_menu_staticmenu_t _1631 = {
+    .u8_numlines = 2,
+    .u8_currentline = 0,
+    .lines = { { "Hello" }, { "1631" } },
+};
+
+// TODO: figure out what goes in about
+static esos_menu_staticmenu_t about = {
+    .u8_numlines = 2,
+    .u8_currentline = 0,
+    .lines = { { "its abt" }, { "time" } },
 };
 
 ESOS_USER_TIMER(heartbeat_LED)
@@ -51,33 +91,84 @@ ESOS_USER_TASK(lcd_example)
 {
     ESOS_TASK_BEGIN();
     while (TRUE) {
-        // Display our main menu until the user presses SW3 to choose a selection
+        // Display main menu until the user presses SW3 to choose a selection
         ESOS_TASK_WAIT_ESOS_MENU_LONGMENU(mm);
-        if (mm.u8_choice < 6)
-            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(err);
+        if (mm.u8_choice == 0)
+            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(wvform);
+        else if (mm.u8_choice == 1)
+            ESOS_TASK_WAIT_ESOS_MENU_ENTRY(freq);
+        else if (mm.u8_choice == 2)
+            ESOS_TASK_WAIT_ESOS_MENU_ENTRY(ampl);
+        else if (mm.u8_choice == 3)
+            ESOS_TASK_WAIT_ESOS_MENU_ENTRY(duty);
+        else if (mm.u8_choice == 4)
+            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(lm60);
+        else if (mm.u8_choice == 5)
+            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(_1631);
         else if (mm.u8_choice == 6)
-            ESOS_TASK_WAIT_ESOS_MENU_ENTRY(ledfp);
+            ESOS_TASK_WAIT_ESOS_MENU_ENTRY(leds);
         else if (mm.u8_choice == 7)
-            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(greet);
-        else if (mm.u8_choice == 8) {
-            ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
-            ESOS_TASK_WAIT_ON_SEND_STRING("Don't procrastinate.\n");
-            ESOS_TASK_WAIT_ON_SEND_STRING("It will not end well for embedded systems students.\n");
-            ESOS_TASK_WAIT_ON_SEND_STRING("Now ... Go make waves\n");
-            ESOS_TASK_WAIT_ON_SEND_STRING("Ba dun tsst\n");
-            ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
-        } else if (mm.u8_choice == 9) {
-        }
+            ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(about);
     }
     ESOS_TASK_END();
 }
 
-ESOS_USER_TASK(flash_led)
+ESOS_USER_TASK(set_led)
 {
     ESOS_TASK_BEGIN();
+    LED1_OFF();
+    LED2_OFF();
+    LED3_HB_OFF();
     while (TRUE) {
-        ESOS_TASK_WAIT_TICKS(ledfp.entries[0].value);
-        LED1 = !LED1;
+        switch (leds.entries[0].value) {
+        case 0:
+            LED1_OFF();
+            LED2_OFF();
+            LED3_HB_OFF();
+            break;
+        case 1:
+            LED1_OFF();
+            LED2_OFF();
+            LED3_HB_ON();
+            break;
+        case 2:
+            LED1_OFF();
+            LED2_ON();
+            LED3_HB_OFF();
+            break;
+        case 3:
+            LED1_OFF();
+            LED2_ON();
+            LED3_HB_ON();
+            break;
+        case 4:
+            LED1_ON();
+            LED2_OFF();
+            LED3_HB_OFF();
+            break;
+        case 5:
+            LED1_ON();
+            LED2_OFF();
+            LED3_HB_ON();
+            break;
+        case 6:
+            LED1_ON();
+            LED2_ON();
+            LED3_HB_OFF();
+            break;
+        case 7:
+            LED1_ON();
+            LED2_ON();
+            LED3_HB_ON();
+            break;
+        }
+
+        // couldn't get this bit manipulation bullshit to work properly
+        // TODO: try again
+        // LED1 = (leds.entries[0].value & 0b100 == 0x100);
+        // LED2 = (leds.entries[0].value & 0b010 == 0x010);
+        // LED3_HB = (leds.entries[0].value & 0b001 == 0x001);
+        ESOS_TASK_YIELD();
     }
     ESOS_TASK_END();
 }
@@ -87,6 +178,6 @@ void user_init()
     // config_esos_uiF14();
     esos_menu_init();
     esos_RegisterTask(lcd_example);
-    esos_RegisterTask(flash_led);
-    esos_RegisterTimer(heartbeat_LED, 500);
+    esos_RegisterTask(set_led);
+    // esos_RegisterTimer(heartbeat_LED, 500);
 }
