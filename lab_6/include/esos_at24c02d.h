68614e50da3d9e1b193669f3c024baa61455cd1a
Will Carroll <woc17@msstate.edu>
2019-03-18 13:15:33 -0500

    eeprom memory programming code

--- lab_6/include/esos_at24c02d.h
+++ lab_6/include/esos_at24c02d.h
@@ -10,6 +10,8 @@
 #include "esos_pic24_i2c.h"
 
 #define AT24C02D_ADDR (0b10100000)
+#define AT24C02D_NUM_PAGES (32)
+#define AT24C02D_PAGE_SIZE (8)
 
 #define ESOS_AT24C02D_WRITE_BYTE(u8_addr, u8_data)                                                                     \
     {                                                                                                                  \
@@ -18,9 +20,9 @@
 
 #define ESOS_AT24C02D_WRITE_PAGE(u8_addr, pu8_data, u8_len)                                                            \
     {                                                                                                                  \
-        uint8_t au8_tmp_data[u8_len + 1];                                                                              \
+        uint8_t u8_i, au8_tmp_data[u8_len + 1];                                                                        \
         au8_tmp_data[0] = u8_addr;                                                                                     \
-        for (uint8_t u8_i = 0; i < u8_len; i++) {                                                                      \
+        for (u8_i = 0; u8_i < u8_len; u8_i++) {                                                                        \
             au8_tmp_data[u8_i + 1] = pu8_data[u8_i];                                                                   \
         }                                                                                                              \
         ESOS_TASK_WAIT_ON_WRITENI2C1(AT24C02D_ADDR, au8_tmp_data, (uint16_t)(u8_len + 1));                             \
--- lab_6/t6_program_eeprom.c
+++ lab_6/t6_program_eeprom.c
@@ -7,8 +7,12 @@
  * t6_program_eeprom.c - Contains EEPROM preprogrammed values and programming code
  */
 
-static const uint8_t[256] au8_EEPROM_DATA = {
-     'S',  'I',  'N',  'E', 0x00, 0x00, 0x00, 0x02, // Page 0 - SINE Name Entry, Scale Factor = (1 / 2)
+#include "esos.h"
+#include "esos_f14ui.h"
+#include "esos_at24c02d.h"
+
+static const uint8_t au8_EEPROM_DATA[AT24C02D_NUM_PAGES * AT24C02D_PAGE_SIZE] = {
+    'S',  'I',  'N',  'E',  0x00, 0x00, 0x00, 0x02, // Page 0 - SINE Name Entry, Scale Factor = (1 / 2)
     0x00, 0x03, 0x06, 0x0A, 0x0D, 0x10, 0x14, 0x17, // Page 1 - SINE 1/4 Wave Entry
     0x1A, 0x1E, 0x21, 0x24, 0x28, 0x2B, 0x2E, 0x32, // Page 2
     0x35, 0x38, 0x3C, 0x3F, 0x42, 0x45, 0x49, 0x4C, // Page 3
@@ -24,7 +28,7 @@ static const uint8_t[256] au8_EEPROM_DATA = {
     0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF7, 0xF8, 0xF9, // Page 13
     0xFA, 0xFA, 0xFB, 0xFB, 0xFC, 0xFC, 0xFD, 0xFD, // Page 14
     0xFD, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFE, 0xFF, // Page 15
-     'N',  'O',  'I',  'S',  'E', 0x00, 0x00, 0x01, // Page 16 - NOISE Name Entry, Scale Factor = (1 / 1)
+    'N',  'O',  'I',  'S',  'E',  0x00, 0x00, 0x01, // Page 16 - NOISE Name Entry, Scale Factor = (1 / 1)
     0xCE, 0x7F, 0x9C, 0x46, 0x1F, 0x2F, 0x89, 0x45, // Page 17 - NOISE 1/4 Wave Entry
     0xA7, 0xB1, 0xB0, 0x53, 0xDE, 0xE4, 0xB6, 0x71, // Page 18
     0x75, 0x6A, 0x83, 0x23, 0xCD, 0x24, 0x50, 0x6C, // Page 19
@@ -41,3 +45,34 @@ static const uint8_t[256] au8_EEPROM_DATA = {
     0x27, 0xB4, 0xAC, 0xDA, 0xE9, 0x99, 0xB2, 0x56, // Page 30
     0x3D, 0x1B, 0x6A, 0x41, 0x24, 0x97, 0x2D, 0xB0, // Page 31
 };
+
+ESOS_USER_TIMER(heartbeat_LED)
+{
+    esos_uiF14_toggleLED3();
+}
+
+ESOS_USER_TASK(program_eeprom)
+{
+    ESOS_TASK_BEGIN();
+
+    uint8_t u8_addr;
+
+    // Write au8_EEPROM_DATA to entire memory
+    for (u8_addr = 0; u8_addr < AT24C02D_NUM_PAGES * AT24C02D_PAGE_SIZE; u8_addr += AT24C02D_PAGE_SIZE) {
+        /*                                             ↓ 1337 h4x below (may not work) */
+        ESOS_AT24C02D_WRITE_PAGE(u8_addr, (uint8_t *)(au8_EEPROM_DATA + u8_addr), AT24C02D_PAGE_SIZE);
+        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+        ESOS_TASK_WAIT_ON_SEND_STRING("\nEEPROM Write Page - Addr: ");
+        ESOS_TASK_WAIT_ON_SEND_UINT8_AS_HEX_STRING(u8_addr);
+        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+    }
+
+    ESOS_TASK_YIELD();
+    ESOS_TASK_END();
+}
+
+void user_init()
+{
+    config_esos_uiF14();
+    esos_RegisterTimer(heartbeat_LED, 500);
+}
