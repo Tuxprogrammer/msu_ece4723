232c9d2313f5bf8831768e9948bc9d491e22f6b3
Christian Bush <chrisbush747@gmail.com>
2019-03-19 21:41:27 -0500

    added fcn_synth file

---
+++ lab_6/fcn_synth.c
@@ -0,0 +1,16 @@
+/* Embedded Systems - Spring 2019
+
+ * Christian Bush (cbb330@msstate.edu), Spencer Callicott (sc2257@msstate.edu)
+ * Will Carroll (woc17@msstate.edu), Landon Casey (lec426@msstate.edu)
+ * Jack Fletcher (jdf469@msstate.edu)
+ *
+ * fcn_synth.c - main user app
+ */
+
+#include "esos_menu.h"
+
+
+void user_init(void)
+{
+    esos_menu_init();
+}
